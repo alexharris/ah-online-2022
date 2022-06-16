40dae74337cccbd346097838ea13260c31f8e12e
alex harris <alexharris6@gmail.com>
2022-06-16 09:37:45 -0700

    add fathom

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
   <!-- <link rel="icon" href="/assets/favicon.png"> -->
+  <script src="https://cdn.usefathom.com/script.js" data-site="ZVIHSYZA" defer></script>
 
   <?php
 
